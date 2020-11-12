ce83df6a336eba97b19cd1664d4e676748a9b4bd
sedykh-ag <55546325+sedykh-ag@users.noreply.github.com>
2020-11-12 18:35:17 +0300

    rewrote rb_tree fully

--- RB_tree/rb_tree.cpp
+++ RB_tree/rb_tree.cpp
@@ -1,356 +1,341 @@
-#include <bits/stdc++.h>
-#include "node.cpp"
+// #pragma once
+#include <iostream>
 
-// дл€ работы непосредственно с указател€ми на узлы
-// удобнее функции пихать не в методы узлов
-// а использовать их как дружественные функции
-// получить родител€ nullptr методом класса -- проблема
+using namespace std;
 
-template<class Key, class T>
-class RBTree {
+enum RBTColor { Black, Red };
+
+template<class KeyType>
+struct  RBTNode
+{
+	KeyType key;
+	RBTColor color;
+	RBTNode<KeyType>* left;
+	RBTNode<KeyType>* right;
+	RBTNode<KeyType>* parent;
+	RBTNode(KeyType _key, RBTColor _color, RBTNode* _parent, RBTNode* _left, RBTNode* _right) :
+		key(_key), color(_color), parent(_parent), left(_left), right(_right) { };
+};
+
+template<class KeyType>
+class  RBTree
+{
 private:
-    /* VARS */
-    Node<Key, T>* root;
+	RBTNode<KeyType>* root;
 
-    /* FUNCTIONS */
+private:
 
-    // Helper functions:
-    Node<Key, T>* GetParent(Node<Key, T>* n) {
-      // Note that parent is set to null for the root node.
-      return n == nullptr ? nullptr : n->parent;
-    }
+	void left_turn(RBTNode<KeyType>*& root, RBTNode<KeyType>* node) {
+		RBTNode<KeyType>* pivot = node->right;
+		node->right = pivot->left;
+		if (pivot->left)
+			pivot->left->parent = node;
 
-    Node<Key, T>* GetGrandParent(Node<Key, T>* n) {
-      // Note that it will return nullptr if this is root or child of root
-      return GetParent(GetParent(n));
-    }
+		pivot->parent = node->parent;
+		if (!node->parent)
+			root = pivot;
+		else {
+			if (node == node->parent->left)
+				node->parent->left = pivot;
+			else
+				node->parent->right = pivot;
+		}
+		pivot->left = node;
+		node->parent = pivot;
+	}
+	void right_turn(RBTNode<KeyType>*& root, RBTNode<KeyType>* node) {
+		RBTNode<KeyType>* pivot = node->left;
+		node->left = pivot->right;
+		if (pivot->right)
+			pivot->right->parent = node;
 
-    Node<Key, T>* GetSibling(Node<Key, T>* n) {
-      Node<Key, T>* p = GetParent(n);
+		pivot->parent = node->parent;
+		if (!node->parent)
+			root = pivot;
+		else
+			if (node == node->parent->right)
+				node->parent->right = pivot;
+			else
+				node->parent->left = pivot;
+		pivot->right = node;
+		node->parent = pivot;
+	}
 
-      // No parent means no sibling.
-      if (p == nullptr) {
-        return nullptr;
-      }
+	void insert(RBTNode<KeyType>*& root, RBTNode<KeyType>* node) {
+		RBTNode<KeyType>* tmp = root;
+		RBTNode<KeyType>* tmp_parent = NULL;
 
-      if (n == p->left) {
-        return p->right;
-      } else {
-        return p->left;
-      }
-    }
+		while (tmp) {
+			tmp_parent = tmp;
+			if (node->key > tmp->key)
+				tmp = tmp->right;
+			else
+				tmp = tmp->left;
+		}
+		node->parent = tmp_parent;
+		if (tmp_parent)
+			if (node->key > tmp_parent->key)
+				tmp_parent->right = node;
+			else
+				tmp_parent->left = node;
+		else
+			root = node;
+		node->color = Red;
+		fix_insertion(root, node);
+	}
+	void fix_insertion(RBTNode<KeyType>*& root, RBTNode<KeyType>* node) {
+		RBTNode<KeyType>* parent;
+		parent = node->parent;
+		while (node != RBTree::root && parent->color == Red) {
+			RBTNode<KeyType>* grandparent = parent->parent;
 
-    Node<Key, T>* GetUncle(Node<Key, T>* n) {
-      Node<Key, T>* p = GetParent(n);
+			if (grandparent->left == parent) {
+				// отец - левый ребЄнок
+				RBTNode<KeyType>* uncle = grandparent->right;
 
-      // No parent means no uncle
-      return GetSibling(p);
-    }
+				if (uncle && uncle->color == Red) {
+					// случай, когда есть д€д€
+					parent->color = Black;
+					uncle->color = Black;
+					grandparent->color = Red;
+					node = grandparent;
+					parent = node->parent;
+				}
+				else {
+					// случай, когда нет д€ди
+					if (parent->right == node) {
+						left_turn(root, parent);
+						swap(node, parent);
+					}
+					right_turn(root, grandparent);
+					grandparent->color = Red;
+					parent->color = Black;
+					break;
+				}
+			}
+			else {
+				// отец - правый ребенок
+				RBTNode<KeyType>* uncle = grandparent->left;
 
-    // Rotations
-    void RotateLeft(Node<Key, T>* n) {
-      Node<Key, T>* nnew = n->right;
-      Node<Key, T>* p = GetParent(n);
-      assert(nnew != nullptr);  // Since the leaves of a red-black tree are empty,
-                                // they cannot become internal nodes.
-      n->right = nnew->left;
-      nnew->left = n;
-      n->parent = nnew;
-      // Handle other child/parent pointers.
-      if (n->right != nullptr) {
-        n->right->parent = n;
-      }
+				if (uncle && uncle->color == Red) {
+					// случай, когда есть д€д€
+					grandparent->color = Red;
+					parent->color = Black;
+					uncle->color = Black;
+					node = grandparent;
+					parent = node->parent;
+				}
+				else {
+					// случай, когда нет д€ди
+					if (parent->left == node) {
+						right_turn(root, parent);
+						swap(parent, node);
+					}
+					left_turn(root, grandparent);
+					parent->color = Black;
+					grandparent->color = Red;
+					break;
+				}
+			}
+		}
+		root->color = Black; // восстанавливаем корень
+	}
 
-      // Initially n could be the root.
-      if (p != nullptr) {
-        if (n == p->left) {
-          p->left = nnew;
-        } else if (n == p->right) {
-          p->right = nnew;
-        }
-      }
-      nnew->parent = p;
-    }
+	void remove(RBTNode<KeyType>*& root, RBTNode<KeyType>* node) {
+		RBTNode<KeyType>* child, *parent;
+		RBTColor color;
 
-    void RotateRight(Node<Key, T>* n) {
-      Node<Key, T>* nnew = n->left;
-      Node<Key, T>* p = GetParent(n);
-      assert(nnew != nullptr);  // Since the leaves of a red-black tree are empty,
-                                // they cannot become internal nodes.
+		//”зел слева и справа от удаленного узла не €вл€етс€ конечным
+		if (node->left && node->right) {
+			RBTNode<KeyType>* replace = node;
 
-      n->left = nnew->right;
-      nnew->right = n;
-      n->parent = nnew;
+			// »щем узел-преемник
+			replace = node->right;
+			while (replace->left)
+				replace = replace->left;
 
-      // Handle other child/parent pointers.
-      if (n->left != nullptr) {
-        n->left->parent = n;
-      }
+			// ”дал€емый узел не €вл€етс€ корневым узлом
+			if (node->parent)
+				if (node->parent->left == node)
+					node->parent->left = replace;
+				else
+					node->parent->right = replace;
+			// ”дал€емый узел €вл€етс€ корневым узлом
+			else
+				root = replace;
 
-      // Initially n could be the root.
-      if (p != nullptr) {
-        if (n == p->left) {
-          p->left = nnew;
-        } else if (n == p->right) {
-          p->right = nnew;
-        }
-      }
-      nnew->parent = p;
-    }
+			child = replace->right;
+			parent = replace->parent;
+			color = replace->color;
 
-    // Insertions
-    Node<Key, T>* Insert(Node<Key, T>* root, Node<Key, T>* n) {
-      // Insert new Node<Key, T> into the current tree.
-      InsertRecurse(root, n);
+			// ”зел замен€етс€ родительским узлом
+			if (parent == node)
+				parent = replace;
+			else {
+				// ƒочерний узел существует
+				if (child)
+					child->parent = parent;
+				parent->left = child;
+				replace->right = node->right;
+				node->right->parent = replace;
+			}
+			replace->parent = node->parent;
+			replace->color = node->color;
+			replace->left = node->left;
+			node->left->parent = replace;
+			// ѕри удалении черной вершины могла быть нарушена балансировка
+			if (color == Black)
+				fix_deleting(root, child, parent);
+			delete node;
+			return;
+		}
+		//  огда удал€емый узел имеет только левый/правый
+		if (node->left)
+			child = node->left;
+		else
+			child = node->right;
 
-      // Repair the tree in case any of the red-black properties have been violated.
-      InsertRepairTree(n);
+		parent = node->parent;
+		color = node->color;
+		if (child)
+			child->parent = parent;
 
-      // Find the new root to return.
-      root = n;
-      while (GetParent(root) != nullptr) {
-        root = GetParent(root);
-      }
-      return root;
-    }
+		// ”дал€емый узел не €вл€етс€ корневым узлом
+		if (parent)
+			if (node == parent->left)
+				parent->left = child;
+			else
+				parent->right = child;
 
-    void InsertRecurse(Node<Key, T>* root, Node<Key, T>* n) {
-      // Recursively descend the tree until a leaf is found.
-      if (root != nullptr)
-      {
-        if (n->key < root->key) {
-          if (root->left != nullptr) {
-            InsertRecurse(root->left, n);
-            return;
-          } else {
-            root->left = n;
-          }
-        } else { // n->key >= root->key
-          if (root->right != nullptr) {
-            InsertRecurse(root->right, n);
-            return;
-          } else {
-            root->right = n;
-          }
-        }
-      }
+		// ”дал€емый узел €вл€етс€ корневым узлом
+		else
+			RBTree::root = child;
 
-      // Insert new Node<Key, T> n.
-      n->parent = root;
-      n->left = nullptr;
-      n->right = nullptr;
-      n->color = RED;
-    }
+		// ѕри удалении черной вершины могла быть нарушена балансировка
+		if (color == Black)
+			fix_deleting(root, child, parent);
+		delete node;
+	}
+	void fix_deleting(RBTNode<KeyType>*& root, RBTNode<KeyType>* node, RBTNode<KeyType>* parent) {
+		RBTNode<KeyType>* brother;
+		while ((!node) || node->color == Black && node != RBTree::root) {
+			if (parent->left == node) {
+				brother = parent->right;
+				if (brother->color == Red) {
+					brother->color = Black;
+					parent->color = Red;
+					left_turn(root, parent);
+					brother = parent->right;
+				}
+				else {
+					if (!(brother->right) || brother->right->color == Black) {
+						brother->left->color = Black;
+						brother->color = Red;
+						right_turn(root, brother);
+						brother = parent->right;
+					}
+					brother->color = parent->color;
+					parent->color = Black;
+					brother->right->color = Black;
+					left_turn(root, parent);
+					node = root;
+					break;
+				}
+			}
+			else {
+				brother = parent->left;
+				if (brother->color == Red) {
+					brother->color = Black;
+					parent->color = Red;
+					right_turn(root, parent);
+					brother = parent->left;
+				}
+				if ((!brother->left || brother->left->color == Black) && (!brother->right || brother->right->color == Black)) {
+					brother->color = Red;
+					node = parent;
+					parent = node->parent;
+				}
+				else {
+					if (!(brother->left) || brother->left->color == Black) {
+						brother->right->color = Black;
+						brother->color = Red;
+						left_turn(root, brother);
+						brother = parent->left;
+					}
+					brother->color = parent->color;
+					parent->color = Black;
+					brother->left->color = Black;
+					right_turn(root, parent);
+					node = root;
+					break;
+				}
+			}
+		}
+		if (node)
+			node->color = Black;
+	}
 
-    void InsertRepairTree(Node<Key, T>* n) {
-      if (GetParent(n) == nullptr) {
-        InsertCase1(n);
-      } else if (GetParent(n)->color == BLACK) {
-        InsertCase2(n);
-      } else if (GetUncle(n) != nullptr && GetUncle(n)->color == RED) {
-        InsertCase3(n);
-      } else {
-        InsertCase4(n);
-      }
-    }
+	void destory(RBTNode<KeyType>*& node) {
+		if (node) {
+			destory(node->left);
+			destory(node->right);
+			delete node;
+			node = nullptr;
+		}
+	}
+	RBTNode<KeyType>* find(RBTNode<KeyType>* node, KeyType key) const {
+		if ((!node) || node->key == key)
+			return node;
+		else
+			return key > node->key ? find(node->right, key) : find(node->left, key);
+	}
 
-    // INSERTION cases
-
-    // Case 1
-    void InsertCase1(Node<Key, T>* n) {
-      n->color = BLACK;
-    }
-
-    // Case 2
-    void InsertCase2(Node<Key, T>* n) {
-      // Do nothing since tree is still valid.
-      return;
-    }
-
-    //Case 3
-    void InsertCase3(Node<Key, T>* n) {
-      GetParent(n)->color = BLACK;
-      GetUncle(n)->color = BLACK;
-      GetGrandParent(n)->color = RED;
-      InsertRepairTree(GetGrandParent(n));
-    }
-
-    //Case 4 step 1
-    void InsertCase4(Node<Key, T>* n) {
-      Node<Key, T>* p = GetParent(n);
-      Node<Key, T>* g = GetGrandParent(n);
-
-      if (n == p->right && p == g->left) {
-        RotateLeft(p);
-        n = n->left;
-      } else if (n == p->left && p == g->right) {
-        RotateRight(p);
-        n = n->right;
-      }
-
-      InsertCase4Step2(n);
-    }
-
-    //Case 4 step 2
-    void InsertCase4Step2(Node<Key, T>* n) {
-      Node<Key, T>* p = GetParent(n);
-      Node<Key, T>* g = GetGrandParent(n);
-
-      if (n == p->left) {
-        RotateRight(g);
-      } else {
-        RotateLeft(g);
-      }
-      p->color = BLACK;
-      g->color = RED;
-    }
-
-    // REMOVAL
-    void ReplaceNode(Node<Key, T>* n, Node<Key, T>* child) {
-      child->parent = n->parent;
-      if (n == n->parent->left) {
-        n->parent->left = child;
-      } else {
-        n->parent->right = child;
-      }
-    }
-
-    void DeleteOneChild(Node<Key, T>* n) {
-      // Precondition: n has at most one non-leaf child.
-      Node<Key, T>* child = (n->right == nullptr) ? n->left : n->right;
-      assert(child);
-
-      ReplaceNode(n, child);
-      if (n->color == BLACK) {
-        if (child->color == RED) {
-          child->color = BLACK;
-        } else {
-          DeleteCase1(child);
-        }
-      }
-      free(n);
-    }
-
-    // REMOVAL cases
-
-    // Case 1
-    void DeleteCase1(Node<Key, T>* n) {
-      if (n->parent != nullptr) {
-        DeleteCase2(n);
-      }
-    }
-
-    // Case 2
-    void DeleteCase2(Node<Key, T>* n) {
-      Node<Key, T>* s = GetSibling(n);
-
-      if (s->color == RED) {
-        n->parent->color = RED;
-        s->color = BLACK;
-        if (n == n->parent->left) {
-          RotateLeft(n->parent);
-        } else {
-          RotateRight(n->parent);
-        }
-      }
-      DeleteCase3(n);
-    }
-
-    // Case 3
-    void DeleteCase3(Node<Key, T>* n) {
-      Node<Key, T>* s = GetSibling(n);
-
-      if ((n->parent->color == BLACK) && (s->color == BLACK) &&
-          (s->left->color == BLACK) && (s->right->color == BLACK)) {
-        s->color = RED;
-        DeleteCase1(n->parent);
-      } else {
-        DeleteCase4(n);
-      }
-    }
-
-    // Case 4
-    void DeleteCase4(Node<Key, T>* n) {
-      Node<Key, T>* s = GetSibling(n);
-
-      if ((n->parent->color == RED) && (s->color == BLACK) &&
-          (s->left->color == BLACK) && (s->right->color == BLACK)) {
-        s->color = RED;
-        n->parent->color = BLACK;
-      } else {
-        DeleteCase5(n);
-      }
-    }
-
-    // Case 5
-    void DeleteCase5(Node<Key, T>* n) {
-      Node<Key, T>* s = GetSibling(n);
-
-      // This if statement is trivial, due to case 2 (even though case 2 changed
-      // the sibling to a sibling's child, the sibling's child can't be red, since
-      // no red parent can have a red child).
-      if (s->color == BLACK) {
-        // The following statements just force the red to be on the left of the
-        // left of the parent, or right of the right, so case six will rotate
-        // correctly.
-        if ((n == n->parent->left) && (s->right->color == BLACK) &&
-            (s->left->color == RED)) {
-          // This last test is trivial too due to cases 2-4.
-          s->color = RED;
-          s->left->color = BLACK;
-          RotateRight(s);
-        } else if ((n == n->parent->right) && (s->left->color == BLACK) &&
-                   (s->right->color == RED)) {
-          // This last test is trivial too due to cases 2-4.
-          s->color = RED;
-          s->right->color = BLACK;
-          RotateLeft(s);
-        }
-      }
-      DeleteCase6(n);
-    }
-
-    // Case 6
-    void DeleteCase6(Node<Key, T>* n) {
-      Node<Key, T>* s = GetSibling(n);
-
-      s->color = n->parent->color;
-      n->parent->color = BLACK;
-
-      if (n == n->parent->left) {
-        s->right->color = BLACK;
-        RotateLeft(n->parent);
-      } else {
-        s->left->color = BLACK;
-        RotateRight(n->parent);
-      }
-    }
+	void print(RBTNode<KeyType>* node) const {
+		if (node) {
+			if (!node->parent)
+				cout << node->key << '[' << node->color << "]	--	root" << endl;
+			else if (node->parent->left == node)
+				cout << node->key << '[' << node->color << "]	--	" << node->parent->key << "'s " << "left child" << endl;
+			else
+				cout << node->key << '[' << node->color << "]	--	" << node->parent->key << "'s " << "right child" << endl;
+			print(node->left);
+			print(node->right);
+		}
+	}
 
 
 public:
-    // Member types
-    using value_type = std::pair<Key, T>;
-    class iterator { }; // FIXME
+	RBTree() : root(nullptr) {}
+	~RBTree() { destory(root); }
 
-    // FINDING
+	void insert(KeyType key) {
+		insert(root, new RBTNode<KeyType>(key, Red, NULL, NULL, NULL));
+	}
 
-    // INSERTION
-    std::pair<iterator, bool> insert( value_type& value ) {
-        std::shared_ptr<Node<Key, T>> n = new Node<Key, T>(value.first, value.second);
-        root = Insert(root, n);
-    }
+	void remove(KeyType key) {
+		RBTNode<KeyType>* deletenode = find(root, key);
+		if (deletenode)
+			remove(root, deletenode);
+	}
 
-    // REMOVAL
-    int erase (Key& key) {
-        // need to use finding first
-    }
+	RBTNode<KeyType>* find(KeyType key) { return find(root, key); }
 
-
-
+	void print() {
+		if (!root)
+			cout << "Empty tree" << '\n';
+		else
+			print(root);
+	}
 
 };
 
+// TESTING
 int main() {
+    RBTree<int> t;
+    t.insert(5);
+    t.insert(1);
+    t.insert(10);
+    RBTNode<int>* ptr = t.find(10);
+    cout << ptr->key;
 
-    return 0;
 }
